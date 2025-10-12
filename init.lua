46c75340acf6553c52f10f10c4ddab0de3d85f50
wsadzanie2 <pawlito2007@gmail.com>
2025-10-12 23:02:48 +0200

    updated config

--- init.lua
+++ init.lua
@@ -853,7 +853,7 @@ require("lazy").setup({
 		--
 		-- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
 		"rebelot/kanagawa.nvim",
-		-- 'folke/tokyonight.nvim',
+		"folke/tokyonight.nvim",
 		priority = 1000, -- Make sure to load this before all the other start plugins.
 		init = function()
 			-- Load the colorscheme here.
